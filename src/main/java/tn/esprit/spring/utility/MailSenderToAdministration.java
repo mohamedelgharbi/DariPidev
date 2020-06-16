6c727cb5e6a4ccd538ca4090f774ce828214050b
hachemwasli <hachem.wasli@esprit.tn>
2020-06-16 18:08:16 +0100

    Hachem Commit 4

---
+++ src/main/java/tn/esprit/spring/utility/MailSenderToAdministration.java
@@ -0,0 +1,66 @@
+package tn.esprit.spring.utility;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author Niyaz-laptop
+ */
+public class MailSenderToAdministration implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void SendMail(String content) {
+		final String username = "dari.simulation@gmail.com";
+		final String password = "dari4119341193";
+		String toMail = "hachem.wasli@esprit.tn";
+		String subject = "Need Administration";
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.socketFactory.port", "465");
+		prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+		Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("dari.simulation@gamil.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toMail));
+			message.setSubject(subject);
+			message.setText(content);
+
+			Transport.send(message);
+
+			System.out.println("Mail Sent To Admin");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
